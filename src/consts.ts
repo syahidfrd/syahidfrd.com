35ce3431e2763949029c213578c1edfcabee041b
syahid <syahidfrd@gmail.com>
2024-09-17 21:45:33 +0700

    imp: arrow card component

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "syahidfrd",
   EMAIL: "syahidfrd@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_WORKS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
